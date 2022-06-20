ec26204118643fb795064ca3bf2668aea0edd1c8
Hyunjin <hyunjin@oddconcepts.kr>
2022-06-21 01:47:49 +0900

    install tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { NextPage } from "next"
 const Home: NextPage = () => {
   return (
     <>
+    <div className="bg-blue-400 text-center">
+      <h1 className="text-black">Hello</h1>
+    </div>
     </>
   )
 }
