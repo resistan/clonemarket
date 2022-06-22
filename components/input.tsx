a4ea8460260ac65471a8d7b2d04192befc4ccb6b
Hyunjin <hyunjin@oddconcepts.kr>
2022-06-23 01:09:08 +0900

    enter form validation

--- components/input.tsx
+++ components/input.tsx
@@ -1,14 +1,24 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface InputProps {
   label: string;
   name: string;
   kind?: "text" | "phone" | "price";
+  register: UseFormRegisterReturn;
+  type: string;
+  required: boolean;
+  disabled?: boolean;
   [key: string]: any;
 }
 
 export default function Input({
   label,
   name,
+  type,
+  required,
   kind = "text",
+  register,
+  disabled,
   ...rest
 }: InputProps) {
   return (
@@ -23,6 +33,10 @@ export default function Input({
         <div className="rounded-md relative flex  items-center shadow-sm">
           <input
             id={name}
+            type={type}
+            required={required}
+            disabled={disabled}
+            {...register}
             {...rest}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
@@ -35,6 +49,10 @@ export default function Input({
           </div>
           <input
             id={name}
+            type={type}
+            required={required}
+            disabled={disabled}
+            {...register}
             {...rest}
             className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
@@ -50,6 +68,10 @@ export default function Input({
           </span>
           <input
             id={name}
+            type={type}
+            required={required}
+            disabled={disabled}
+            {...register}
             {...rest}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
