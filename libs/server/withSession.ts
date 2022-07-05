3961d5788616fc68fc166eb9a7be3f8605939ef9
Hyunjin <hyunjin@oddconcepts.kr>
2022-07-05 18:03:38 +0900

    SSR with Session Auth

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,18 +1,22 @@
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 declare module "iron-session" {
-	interface IronSessionData {
-		user?: {
-			id: number;
-		}
-	}
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
 }
 
 const cookieOptions = {
-	cookieName:"clonemarket",
-	password: process.env.COOKIE_PASSWORD!,
+  cookieName: "clonemarket",
+  password: process.env.COOKIE_PASSWORD!,
+};
+
+export function withApiSession(fn: any) {
+  return withIronSessionApiRoute(fn, cookieOptions);
 }
 
-export function withApiSession(fn:any){
-	return withIronSessionApiRoute(fn, cookieOptions)
+export function withSsrSession(handler: any) {
+  return withIronSessionSsr(handler, cookieOptions);
 }
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,14 +1,16 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Link from "next/link";
 import Layout from "@components/layout";
 import useMutation from "@libs/client/useMutation";
 import useUser from "@libs/client/useUser";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { Review, User } from "@prisma/client";
 import { cfimg, cls } from "@libs/client/utils";
 import Image from "next/image";
+import { withSsrSession } from "@libs/server/withSession";
+import client from "@libs/server/prismaClient";
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -18,9 +20,51 @@ interface IReviewsResponse {
   reviews: ReviewWithUser[];
 }
 
-const Profile: NextPage = () => {
-  const { user, isLoading } = useUser();
+const Reviews = () => {
   const { data: reviewData } = useSWR<IReviewsResponse>("/api/reviews");
+  return (
+    <>
+      {reviewData?.reviews.map((review) => (
+        <div className="mt-12" key={review.id}>
+          <div className="flex space-x-4 items-center">
+            <div className="w-12 h-12 rounded-full bg-slate-500" />
+            <div>
+              <h4 className="text-sm font-bold text-gray-800">
+                {review.createdBy.name}
+              </h4>
+              <div
+                className="flex items-center"
+                aria-label={`${review.score} / 5점`}
+              >
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <svg
+                    key={star}
+                    className={cls(
+                      "h-5 w-5",
+                      review.score >= star ? "text-yellow-400" : "text-gray-400"
+                    )}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 text-gray-600 text-sm">
+            <p>{review.review}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const Profile: NextPage = () => {
+  const { user } = useUser();
   const router = useRouter();
   const [logoff, { data: loggedInData }] = useMutation("/api/users/logout");
   const onLogoutClick = () => {
@@ -48,7 +92,7 @@ const Profile: NextPage = () => {
           )}
           <div className="flex flex-col">
             <span className="font-medium text-gray-900">
-              {user?.name} {user && user?.isAdmin && "(관리자)"}
+              {user?.name} {user?.isAdmin && "(관리자)"}
             </span>
             <Link href="/profile/edit">
               <a className="text-sm text-gray-700">Edit profile &rarr;</a>
@@ -137,45 +181,41 @@ const Profile: NextPage = () => {
             </a>
           </Link>
         </div>
-        {reviewData?.reviews.map((review) => (
-          <>
-            <div className="mt-12" key={review.id}>
-              <div className="flex space-x-4 items-center">
-                <div className="w-12 h-12 rounded-full bg-slate-500" />
-                <div>
-                  <h4 className="text-sm font-bold text-gray-800">
-                    {review.createdBy.name}
-                  </h4>
-                  <div className="flex items-center">
-                    {[1, 2, 3, 4, 5].map((star) => (
-                      <svg
-                        key={star}
-                        className={cls(
-                          "h-5 w-5",
-                          review.score >= star
-                            ? "text-yellow-400"
-                            : "text-gray-400"
-                        )}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="mt-4 text-gray-600 text-sm">
-                <p>{review.review}</p>
-              </div>
-            </div>
-          </>
-        ))}
+        <Reviews />
       </div>
     </Layout>
   );
 };
 
-export default Profile;
+const Page: NextPage<{ profile: User }> = ({ profile }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          "/api/users/me": {
+            ok: true,
+            profile,
+          },
+        },
+      }}
+    >
+      <Profile />
+    </SWRConfig>
+  );
+};
+
+export const getServerSideProps = withSsrSession(async function ({
+  req,
+}: NextPageContext) {
+  // console.log(req?.session.user);
+  const profile = await client.user.findUnique({
+    where: { id: req?.session.user?.id },
+  });
+  return {
+    props: {
+      profile: JSON.parse(JSON.stringify(profile)),
+    },
+  };
+});
+
+export default Page;
