0398520627a393d9a8a51e6eafd7c560b7ccb0b9
Hyunjin <hyunjin@oddconcepts.kr>
2022-07-12 09:47:05 +0900

    fix middelware

--- middleware.ts
+++ middleware.ts
@@ -7,24 +7,24 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   //   return new Response("Interrupted. Be a human", { status: 403 });
   // }
   // console.log(req.cookies.get("clonemarket"));
-  if (!req.nextUrl.pathname.startsWith("/api")) {
-    if (
-      !req.nextUrl.pathname.startsWith("/enter") &&
-      !req.cookies.get("clonemarket")
-    ) {
-      return NextResponse.redirect(new URL("/enter", req.url));
-    }
-    if (
-      !req.nextUrl.pathname.startsWith("/") &&
-      req.cookies.get("clonemarket")
-    ) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-  }
+  // if (!req.nextUrl.pathname.startsWith("/api")) {
+  //   if (
+  //     !req.nextUrl.pathname.startsWith("/enter") &&
+  //     !req.cookies.get("clonemarket")
+  //   ) {
+  //     return NextResponse.redirect(new URL("/enter", req.url));
+  //   }
+  //   if (
+  //     !req.nextUrl.pathname.startsWith("/") &&
+  //     req.cookies.get("clonemarket")
+  //   ) {
+  //     return NextResponse.redirect(new URL("/", req.url));
+  //   }
+  // }
   // console.log(req.geo?.country); // not work on localhost
 }
 
 // Supports both a single string value or an array of matchers
-export const config = {
-  // matcher: ["/about/:path*", "/dashboard/:path*"],
-};
+// export const config = {
+//   // matcher: ["/about/:path*", "/dashboard/:path*"],
+// };
